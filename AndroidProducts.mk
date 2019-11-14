deaeadb0ce13f2bacbfd073ea4f42222bb586b7d
Art_Chen <chenxy0201@qq.com>
2019-11-14 13:50:05 +0800

    RMX1901: mk -> mokee

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/mk_RMX1901.mk
+    $(LOCAL_DIR)/mokee_RMX1901.mk
+
+COMMON_LUNCH_CHOICES := \
+    mokee_RMX1901-user \
+    mokee_RMX1901-userdebug \
+    mokee_RMX1901-eng
